0b6d345ae8ebb1073cdfa7dca7b887aa8f03a387
arty1226 <artur1226@gmail.com>
2021-12-23 00:04:07 +0200

    Add files via upload

---
+++ Lab5/scr/app/services/voter.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VoterService {
+    constructor(private http: HttpClient) { }
+    apiUrl = "https://randomuser.me/api/?results=10";
+
+    getVoters(): Observable<any>{
+        return this.http.get<any[]>(this.apiUrl);
+    }
+}
